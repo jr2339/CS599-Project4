499df512a57aa44db1ce00ad5ad040f2fe4cc7aa
jr2339 <jr2339@nau.edu>
2016-11-07 23:07:15 -0700

    Init

---
+++ RayTracing/RayTracing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  RayTracing
+//
+//  Created by jr2339 on 11/7/16.
+//  Copyright © 2016 jr2339. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
